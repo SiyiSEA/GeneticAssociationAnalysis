46834bbfede145c3f6e0737465e3de23d3a4a151
Siyi Wang <s.w.wang@exeter.ac.uk>
2025-07-04 14:59:07 +0100

    developing

--- 03-COJO.sh
+++ 03-COJO.sh
@@ -10,7 +10,7 @@
 #SBATCH --mem=40G
 #SBATCH --ntasks=16
 #SBATCH --time=0-20:00:00
-#SBATCH --array=7
+#SBATCH --array=1-6
 
 #####################################################################################################################
 
@@ -18,16 +18,16 @@
 
 module purge
 module load R/4.2.1-foss-2022a
-source ./config "${SLURM_ARRAY_TASK_ID}"
+source $1 "${SLURM_ARRAY_TASK_ID}"
 timetemp=$(date -u +%Y-%m-%d_%H-%M)
-exec &> >(tee ./COJOresults/logs/03COJO_log${timetemp}_${phenotype}.log)
-
+exec &> >(tee ${COJOresPath}/logs/03COJO_log${timetemp}_${phenotype}.log)
 
 #### COJO 
 # select independently associated SNPs
 cd  "${COJOresPath}"/Cojo_"${phenotype}" || exit
 
-gcta-1.94.1 --bfile ${HomePath}/Resources/1000Geur/1000G_hg19_eur \
+# !!!!!! 1000Geur no ChrX!!!!
+gcta-1.94.1 --bfile ${ScriptsPath}/Resources/1000Geur/1000G_hg19_eur \
             --cojo-file ${METAresPath}/FilteredMeta/"${phenotype}"_input.cojo \
             --cojo-slct \
             --cojo-p 0.00001 \
@@ -44,99 +44,72 @@ Rscript ${RscriptsPath}/plot_ld.R \
                         "${phenotype}".ldr.cojo \
                         "${phenotype}"
 
+echo "Please check the LD plots for each ${phenotype} in the ${COJOresPath}/Cojo_${phenotype} directory."
+echo "Making sure there are common signals for each pair of phenotypes."
 
-# # Version-1: Pick the SNPs based on the BP(+-5kb) of independent SNPs identified by COJO from metaed data
-# singal_Chr=$(tail -n +2 "${phenotype}".jma.cojo | cut -f1 | sort -nu)
-# for chr in $singal_Chr; do
-#   echo "split the cma.cojo by Chromosome: $chr"
-#   echo "SNP BP" > Chr"${chr}"_cma.cojo
-#   awk '{print $2,$3}' "${phenotype}".cma.cojo | grep "^$chr:" >> Chr"${chr}"_cma.cojo
-#   echo "split the jma.cojo by Chromosome: $chr"
-#   echo "SNP BP" > Chr"${chr}"_jma.cojo
-#   awk '{print $2,$3}' "${phenotype}".jma.cojo | grep "^$chr:" >> Chr"${chr}"_jma.cojo
+# Pick the independent SNPs from the meta-analysis results
+awk '{print $2}' $phenotype.jma.cojo | tail -n +2  > SignalCojoSNP.txt.temp
 
-#   Rscript ${RscriptsPath}/pick_SNPs.R \
-#                 ${COJOresPath}/Cojo_"${phenotype}" \
-#                 Chr"${chr}"_cma.cojo \
-#                 Chr"${chr}"_jma.cojo
-# done
+for snp in "${tartgetSNPs[@]}"; do
+    echo "$snp" >> SignalCojoSNP.txt.temp
+done
 
-# cat Chr*CojoSNPlist.txt > SuperCojoSNP.txt
-# rm Chr*CojoSNPlist.txt
-# rm Chr*.cojo
-
-# Version-2: Pick the independent SNPs from the meta-analysis results
-awk '{print $2}' $phenotype.jma.cojo | tail -n +2  > SignalCojoSNP.Mpre
-awk '{print $2}' $phenotype.jma.cojo | tail -n +2  > SignalCojoSNP.forestpre.temp
-
-if [ "$phenotype" = "DNAmAgeSD" ] || [ "$phenotype" = "DNAmAgessSD" ]; then
-    {
-        echo "6:18130918_C_T"
-        echo "6:18120400_C_T"
-    } >> SignalCojoSNP.forestpre.temp
-elif [ "$phenotype" = "PhenoAgeSD" ] || [ "$phenotype" = "PhenoAgessSD" ]; then
-    {
-        echo "6:18130918_C_T"
-        echo "6:18109815_C_G"
-    } >> SignalCojoSNP.forestpre.temp
-     
-elif [ "$phenotype" = "DunedinPACESD" ] || [ "$phenotype" = "DunedinPACEssSD" ]; then
-    {
-        echo "14:74141429_A_G"
-        echo "14:74176679_C_T"
-        echo "12:125342972_A_G"
-    } >> SignalCojoSNP.forestpre.temp
-
-elif [ "$phenotype" = "gwas_smoking" ]; then
-    {
-        echo "3:128321226_A_G"
-        echo "3:22585598_C_T"
-        echo "15:75052495_C_T"
-        echo "19:58776215_A_T"
-    } >> SignalCojoSNP.forestpre.temp
-
-else
-    echo "No specific SNPs for forest plot in ${phenotype}."
-    exit
-fi
-
-sort -u SignalCojoSNP.forestpre.temp > SignalCojoSNP.forestpre
-
+sort -u SignalCojoSNP.txt.temp > SignalCojoSNPlist.txt
+rm SignalCojoSNP.txt.temp
 
 # From each cohort, extract the SNPs in the SuperCojoSNP.txt
 # the cohort sets can be modified in the config file
 echo "Cohort fastGWA_N" > "${phenotype}"_chorts.fastGWA_N
 
-cohorts=$(grep 'StatsAgeSmoke' ${HomePath}/METALresults/SampleScheme/${phenotype}_SS1.tbl.info | awk -F'/' '{ print $2 }')
-for cohort in $cohorts
+cohortGWAs=$(grep "Input File" ${METAresPath}/SampleScheme/${phenotype}_SS1.tbl.info | cut -d':' -f2- | sed 's/^ *//')
+
+for cohortGWA in $cohortGWAs
 do
-  echo "Grepping signal SNPs from ${cohort}"
-  cohortGWA="${HomePath}/StatsAgeSmoke/${cohort}/${fastGWA}"
-  awk -F $'\t' -v OFS=$'\t' -v name=${cohort} 'NR==1  { print $0, "Study"; next } { print $0, name } ' ${cohortGWA} > temp.fastGWA
+  echo "Grepping signal SNPs from ${cohortGWA}"
+  cohortName=$(echo "${cohortGWA}" | awk -F'/' '{print $(NF-3)}')
+  echo "Cohort name: ${cohortName}"
+  awk -F $'\t' -v OFS=$'\t' -v name=${cohortName} 'NR==1  { print $0, "Study"; next } { print $0, name } ' ${cohortGWA} > temp.fastGWA
   
   # append any lines matching exactly one of the SNPs
-  grep -Ff SignalCojoSNP.Mpre temp.fastGWA | tail -n +2 > "${cohort}"_"${phenotype}"_tempMpre.fastGWA
-  grep -Ff SignalCojoSNP.forestpre temp.fastGWA | tail -n +2 > "${cohort}"_"${phenotype}"_tempforestpre.fastGWA
+  grep -Ff SignalCojoSNPlist.txt temp.fastGWA | tail -n +2 > "${cohortName}"_"${phenotype}"_temp.fastGWA
 
   samplesize=$(awk '{print $6}' temp.fastGWA | tail -n 1)
-  echo "Counting the sample size: ${cohort} ${samplesize}"
-  echo "${cohort} ${samplesize}" >> "${phenotype}"_chorts.fastGWA_N
+  echo "Counting the sample size: ${cohortName} ${samplesize}"
+  echo "${cohortName} ${samplesize}" >> "${phenotype}"_chorts.fastGWA_N
 done
 
-head -n1 temp.fastGWA > "${MstatresPath}"/Mstat_"${phenotype}"/SignalSNP.Mstat.pre
-head -n1 temp.fastGWA > "${MstatresPath}"/Mstat_"${phenotype}"/SignalSNP.forest.pre
+head -n1 temp.fastGWA > "${MstatresPath}"/Mstat_"${phenotype}"/SignalSNPs.pre
 
-cat *${phenotype}_tempMpre.fastGWA >> "${MstatresPath}"/Mstat_"${phenotype}"/SignalSNP.Mstat.pre
-cat *${phenotype}_tempforestpre.fastGWA >> "${MstatresPath}"/Mstat_"${phenotype}"/SignalSNP.forest.pre
+cat *${phenotype}_temp.fastGWA >> "${MstatresPath}"/Mstat_"${phenotype}"/SignalSNPs.pre
 mv "${phenotype}"_chorts.fastGWA_N "${MstatresPath}"/Mstat_"${phenotype}"/"${phenotype}"_chorts.fastGWA_N
 rm *${phenotype}_temp*.fastGWA
+rm temp.fastGWA
+
+echo "Checking if the SignalSNPs.pre file is created successfully..."
+num_lines=$(wc -l < "${MstatresPath}"/Mstat_"${phenotype}"/SignalSNPs.pre)
+num_cohort=$(wc -l < "${MstatresPath}"/Mstat_"${phenotype}"/"${phenotype}"_chorts.fastGWA_N)
+if [ $num_lines -gt $num_cohort ]; then
+    echo "SignalSNPs.pre file created successfully with ${num_lines} lines."
+else
+    echo "Error: SignalSNPs.pre is failed !"
+    exit 1
+fi
 
 
+# information
+echo " "
 echo "==========================Information of ${phenotype}======================"
 echo "The total number of Sig SNPs detected by COJO of ${phenotype} is "
 wc -l "${phenotype}".jma.cojo
 echo "How many bad SNPs doesn't match to the 1000G eur reference file? "
 wc -l ${phenotype}.freq.badsnps
+echo "The output files from the script are:"
+echo "1.${phenotype}.jma.cojo: independent SNPs selected by COJO"
+echo "2.${phenotype}.cma.cojo: the rest of the SNPs"
+echo "3.${phenotype}.ldr.cojo: LD correlation matrix between all pairwise SNPs listed in ${phenotype}.jma.cojo"
+echo "4.SignalCojoSNPlist.txt: the list of independent SNPs selected by COJO and the target SNPs"
+echo "5.Mstat_"${phenotype}"/SignalSNPs.pre: the list of signal SNPs from all cohorts"
+echo "6.Mstat_"${phenotype}"/"${phenotype}"_chorts.fastGWA_N: the list of cohorts and their sample sizes"
 echo "============================================================================"
 
 echo "Successfully completed the COJO for ${phenotype}."
